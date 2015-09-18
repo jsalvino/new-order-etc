8fdfbca7b772dc22dddd89dea9d8a79f53597e3f
John Salvino <jsalvino@me.com>
2015-09-18 11:32:38 -0400

    Basic template pages setup

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
   <div class="container">
-    <p>&copy; HackerYou <?php echo date('Y'); ?></p>
+    <p>&copy; John Salvino <?php echo date('Y'); ?></p>
   </div>
 </footer>
 
---
+++ page-home.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+	Template Name: Home
+*/
+add_action( 'wp_head', 'vr_set_featured_background', 99);
+  function vr_set_featured_background() {
+  $image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), full, false );
+ if ($image_url[0]) {
+   ?>
+     <style>
+     
+       html,body {
+       height:100%;
+       margin:0!important;
+       }
+       body {
+       background:url(<?php echo $image_url[0]; ?>) #fff center top no-repeat;
+       background-size: cover;
+       }
+     </style>
+   <?php
+ }  //end if statement
+} //end vr_set_featured_background() function
+
+get_header();  ?>
+
+<div class="main">
+  <div class="container">
+
+    <?php // Start the loop ?>
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+      <h2><?php //the_title(); ?></h2>
+      <?php the_content(); ?>
+
+    <?php endwhile; // end the loop?>
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
