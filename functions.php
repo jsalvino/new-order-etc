59337f87005b7ebb77fc8d2cf91bded3a7107153
John Salvino <jsalvino@me.com>
2015-09-23 18:09:49 -0400

    Added wow.js and animate.css for Album page

--- functions.php
+++ functions.php
@@ -266,3 +266,34 @@ function get_post_parent($post) {
 		return $post->ID;
 	}
 }
+
+// Add wow.js and animate.css
+//* Enqueue Animate.CSS and WOW.js
+add_action( 'wp_enqueue_scripts', 'sk_enqueue_scripts' );
+function sk_enqueue_scripts() {
+ 
+
+	wp_enqueue_style( 'animate', get_stylesheet_directory_uri() . '/css/animate.min.css' );
+ 
+
+	wp_enqueue_script( 'wow', get_stylesheet_directory_uri() . '/js/wow.min.js', array(), '', true );
+ 
+
+}
+ 
+
+//* Enqueue script to activate WOW.js
+add_action('wp_enqueue_scripts', 'sk_wow_init_in_footer');
+function sk_wow_init_in_footer() {
+	add_action( 'print_footer_scripts', 'wow_init' );
+}
+ 
+
+//* Add JavaScript before </body>
+function wow_init() { ?>
+	<script type="text/javascript">
+		new WOW().init();
+	</script>
+<?php }
+
+
--- page-albums.php
+++ page-albums.php
@@ -57,10 +57,10 @@ get_header();  ?>
       
         <!-- <section id="<?php echo $post->post_name; ?>"> -->
         <div class="album-item <?php echo $post->post_name; ?>">
-          <div class="album-image">
+          <div class="wow bounceInLeft album-image">
             <p><?php the_post_thumbnail('large'); ?></p>
           </div> <!-- album-image -->
-          <div class="album-info">
+          <div class="wow bounceInRight album-info">
             <h2><?php the_title(); ?></h2>
             <p><?php the_field('album_short_description'); ?></p>
             <p><?php the_field('album_rating'); ?> out of 5 stars</p>
